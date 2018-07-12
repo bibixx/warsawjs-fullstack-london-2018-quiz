4c6ee24c76fb627e7304cea00c051194bd7ebeff
Kacper Pietrzak <kacpietrzak@gmail.com>
2018-07-12 13:45:19 +0200

    Tests, tests, tests

--- challange/index.spec.js
+++ challange/index.spec.js
@@ -9,7 +9,7 @@ it("returned array should have same length as input if all parameters are valid"
   expect(val).to.have.lengthOf(params.length);
 });
 
-it("returned array should not contain Invalid Dates", () => { 
+it("returned array should not contain Invalid Dates", () => {
   const params = ["Thu, 02 Jan 1970 00:00:00 GMT-0400", "Wed, 01 Jan 1970 00:00:00 GMT-0400", "Thu Jan 01 1970 01:00:00 GMT+0100", "WarsawJS"]
   const val = fn(params);
 
@@ -17,7 +17,12 @@ it("returned array should not contain Invalid Dates", () => {
 });
 
 it("returned array should be sorted properly", () => {
-  const val = fn(["Thu, 01 Jan 1970 00:00:00 GMT-0400", "Wed, 02 Jan 1970 00:00:00 GMT-0400"]);
+  const params = ["Thu, 02 Jan 1970 00:00:00 GMT-0400", "Wed, 01 Jan 1970 00:00:00 GMT-0400", "Thu Jan 01 1970 01:00:00 GMT+0100"];
+  const expectedOutput = ["1970-01-01T00:00:00.000Z", "1970-01-01T04:00:00.000Z", "1970-01-02T04:00:00.000Z"];
+  const val = fn(params);
 
-  expect(JSON.stringify(val)).to.be.equal('["1970-01-01T04:00:00.000Z","1970-01-02T04:00:00.000Z"]');
+  val.forEach((v, i) => {
+    expect(v.getTime()).to.be.equal(new Date(expectedOutput[i]).getTime())
+  });
+
 });
